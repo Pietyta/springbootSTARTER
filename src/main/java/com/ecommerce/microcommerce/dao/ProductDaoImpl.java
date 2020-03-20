8275029e1bbf9d92b678cf7e2982a9fccd70ba41
piety <31342349+Pietyta@users.noreply.github.com>
2020-03-20 03:21:58 +0100

    Add files via upload

---
+++ src/main/java/com/ecommerce/microcommerce/dao/ProductDaoImpl.java
@@ -0,0 +1,21 @@
+package com.ecommerce.microcommerce.dao;
+import com.ecommerce.microcommerce.model.Product;
+import java.util.List;
+
+public class ProductDaoImpl implements ProductDao {
+
+    @Override
+    publicList<Product>findAll() {
+        return null;
+    }
+
+    @Override
+    publicProductfindById(int id) {
+        return null;
+    }
+
+    @Override
+    publicProductsave(Product product) {
+        return null;
+    }
+}
